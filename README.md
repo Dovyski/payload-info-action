48f0a4d35c31441021a6e8afa6cddf0a11c97ea4
Fernando Bevilacqua <dovyski@gmail.com>
2020-01-16 15:40:32 -0300

    Add initial structure

---
+++ README.md
@@ -0,0 +1,49 @@
+# Payload Info
+
+This action extracts info from the webhook payload object in Github Actions using the `jq` tool. If also provides the name of the branch this workflow is running on and if it is a pull request or not. 
+
+## Inputs
+
+### `filter_push`
+**Required** Filter to be applied to the webhook payload object in case of a push. Default: `"."`. Learn more about filters at the [jq manual](https://stedolan.github.io/jq/manual/#Basicfilters).
+
+### `filter_pull_request`
+Filter to be applied to the webhook payload object in case of a pull request. Default `".pull_request."`. Learn more about filters at the [jq manual](https://stedolan.github.io/jq/manual/#Basicfilters).
+
+### `output`
+Type of output. It can be `"pretty"`, `"json"`, `"compact"`, or `"string"`. Default `"pretty"`. ''
+
+### `slurp`
+If the output will be surrounded between `[` and `]`. Default `false`.
+
+### `sort`
+Sort object keys in alphabetical order. Default `false`.
+
+### `dump`
+If the webhook payload for the event that triggered the workflow should be dumped as a JSON file or not.' Default `true`.
+
+### `dump_path`
+Path to the JSON payload file to be dumped. Default `"./gh-payload.json"`.
+
+### `print`
+If the webhook payload for the event that triggered the workflow should be output. Default `true`.
+
+## Outputs
+
+### `value`
+Result after the filter is applied.
+
+### `branch`
+Name of the branch related to this workflow.
+
+### `pull_request`
+If this workflow is running on a pull request or not.
+
+## Example usage
+
+```yml
+uses: Dovyski/payload-info-action@master
+with:
+  filter_push: '.'
+  filter_pull_request: '.pull_request.'
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const jq = require('node-jq');
+const fs = require('fs');
+
+try {
+    const filterPush        = core.getInput('filter_push');
+    const filterPullRequest = core.getInput('filter_pull_request');
+    const shouldDump        = core.getInput('dump');
+    const dumpPath          = core.getInput('dump_path');
+    const shouldPrint       = core.getInput('print');
+    const output            = core.getInput('output');
+    const slurp             = core.getInput('slurp');
+    const sort              = core.getInput('sort');
+
+    const isPullRequest     = github.context.payload.pull_request !== undefined;
+    const branch            = isPullRequest ? github.context.payload.pull_request.head.ref : github.context.payload.ref;
+    const filter            = isPullRequest ? filterPullRequest : filterPush;
+    
+    core.setOutput('pull_request', JSON.stringify(isPullRequest));
+    core.setOutput('branch', branch);
+
+    jq.run(filter, github.context.payload, {
+        input: 'json',
+        output: output || 'pretty',
+        slurp: Boolean(slurp),
+        sort: Boolean(sort)
+    })
+    .then((output) => {
+        core.setOutput('value', output);
+    })
+    .catch((err) => {
+        core.setFailed(err);
+    });
+        
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+    if(shouldPrint) {
+        console.log(`Payload: ${payload}`);
+    }
+
+    if(shouldDump) {
+        fs.writeFile(dumpPath, payload, function(err) {
+            if(err) {
+                core.setFailed(err);
+            }
+            console.log(`Payload dump file saved at "${dumpPath}".`);
+        });
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
